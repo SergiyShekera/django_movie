5c9b5b130d1331531065ae5ff7f456f77db7aa95
SergiyShekera <sergeyshekera98@gmail.com>
2020-05-16 21:28:55 +0300

    Register models in admin site

--- movies/admin.py
+++ movies/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Category
+from .models import Genre
+from .models import Movie
+from .models import MovieShots
+from .models import Actor
+from .models import Rating
+from .models import RatingStar
+from .models import Reviews
+
+admin.site.register(Category)
+admin.site.register(Genre)
+admin.site.register(Movie)
+admin.site.register(MovieShots)
+admin.site.register(Actor)
+admin.site.register(Rating)
+admin.site.register(RatingStar)
+admin.site.register(Reviews)
