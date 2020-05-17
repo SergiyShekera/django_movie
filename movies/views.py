953775245cc7b260a24c2c2e3cd4d58fadeb1019
SergiyShekera <sergeyshekera98@gmail.com>
2020-05-17 17:25:49 +0300

    Updated movies.views by use ListView, DetailView

--- movies/views.py
+++ movies/views.py
@@ -1,18 +1,15 @@
-from django.shortcuts import render
-from django.views.generic.base import View
+from django.views.generic import ListView, DetailView
 
 from .models import Movie
 
 
-class MovieView(View):
+class MoviesView(ListView):
     """Список фильмов"""
-    def get(self, request):
-        movies = Movie.objects.all()
-        return render(request, "movie/movie_list.html", {"movie_list": movies})
+    model = Movie
+    queryset = Movie.objects.filter(draft=False)
 
 
-class MovieDetailView(View):
+class MovieDetailView(DetailView):
     """Полное описание фильма"""
-    def get(self, request, slug):
-        movie = Movie.objects.get(url=slug)
-        return render(request, "movie/movie_detail.html", {"movie": movie})
+    model = Movie
+    slug_field = "url"
