23f163ac48575c3e71939a891099647d8f610c62
SergiyShekera <sergeyshekera98@gmail.com>
2020-05-17 16:43:08 +0300

    Added MovieDetailView, movie/movie_detail.html, get movie detail by movie slug(Movie.url)

--- movies/views.py
+++ movies/views.py
@@ -9,3 +9,10 @@ class MovieView(View):
     def get(self, request):
         movies = Movie.objects.all()
         return render(request, "movie/movies.html", {"movie_list": movies})
+
+
+class MovieDetailView(View):
+    """Полное описание фильма"""
+    def get(self, request, slug):
+        movie = Movie.objects.get(url=slug)
+        return render(request, "movie/movie_detail.html", {"movie": movie})
