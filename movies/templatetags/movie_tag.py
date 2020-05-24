32bc884e8caeaad7df3d54e4a146dea9f8cfba5a
SergiyShekera <sergeyshekera98@gmail.com>
2020-05-24 19:55:03 +0300

    Added inclusion_tag last_movies

--- movies/templatetags/movie_tag.py
+++ movies/templatetags/movie_tag.py
@@ -9,3 +9,9 @@ register = template.Library()
 def get_categories():
     """Вывод всех категорий"""
     return Category.objects.all()
+
+
+@register.inclusion_tag("movies/tags/last_movie.html")
+def get_last_movies(count=10):
+    movies = Movie.objects.order_by("id")[:count]
+    return {"last_movies": movies}
